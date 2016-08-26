9c9ed4acfe63d20da0729afd354dc145569432fd
Stefan Fritsch <stefan.fritsch@stat-up.com>
2016-08-26 10:58:10 +0200

    Fix-Versuch 1

--- rstudio-statup/tools/ChocolateyInstall.ps1
+++ rstudio-statup/tools/ChocolateyInstall.ps1
@@ -5,11 +5,10 @@
 		silentArgs = '/S'
 		url64bit = 'https://s3.amazonaws.com/rstudio-dailybuilds/RStudio-0.99.1292.exe'
 		checksum64 = 'af3d1af96bf117ca941dd8aeb097fc1c355a1108519ddf8e9cbc2cef54097e56'
-#		checksumType = 'sha256'
+		checksumType64 = 'sha256'
 	}
 	
 	Install-ChocolateyPackage @packageArgs
-	# $packageName $fileType $silentArgs $url
 
     'Successfully installed rstudio-statup'
 } catch {
